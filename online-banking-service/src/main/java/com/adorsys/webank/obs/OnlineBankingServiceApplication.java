378574da37fcfc5b9b2b9c481cc770bd314e37ec
Nancy Muyeh <nancymuyeh@gmail.com>
2024-09-23 13:12:51 +0100

    Initialize basic project structure for online banking

---
+++ online-banking-service/src/main/java/com/adorsys/webank/obs/OnlineBankingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.adorsys.webank.obs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineBankingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineBankingServiceApplication.class, args);
+	}
+
+}
---
+++ online-banking-service/src/test/java/com/adorsys/webank/obs/OnlineBankingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adorsys.webank.obs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineBankingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
