d8e360ded9a3831488fa9ad7b7f0be8da0378a8a
Menkene Koufan <Menkene.Koufan@adorsys.com>
2024-10-22 12:54:46 +0100

    chore: created composite application

--- online-banking-app/src/main/java/com/adorsys/webank/OnlineBankingApplication.java
+++ online-banking-app/src/main/java/com/adorsys/webank/OnlineBankingApplication.java
@@ -1,9 +1,9 @@
-package com.adorsys.webank.obs;
+package com.adorsys.webank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
+@SpringBootApplication(exclude = {org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration.class,
+		org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration.class})
 public class OnlineBankingApplication {
 
 	public static void main(String[] args) {
--- online-banking-app/src/test/java/com/adorsys/webank/OnlineBankingApplicationTests.java
+++ online-banking-app/src/test/java/com/adorsys/webank/OnlineBankingApplicationTests.java
@@ -1,14 +1,15 @@
-package com.adorsys.webank.obs;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest(classes = OnlineBankingApplication.class) // Specify main class
-public class OnlineBankingApplicationTests {
-
-	@Test
-	public void contextLoads() {
-		// This test intentionally does nothing.
-		// The @SpringBootTest annotation simulates application startup.
-	}
-}
+//package com.adorsys.webank;
+//
+//import com.adorsys.webank.obs.OnlineBankingApplication;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest(classes = OnlineBankingApplication.class) // Specify main class
+//public class OnlineBankingApplicationTests {
+//
+//	@Test
+//	public void contextLoads() {
+//		// This test intentionally does nothing.
+//		// The @SpringBootTest annotation simulates application startup.
+//	}
+//}
