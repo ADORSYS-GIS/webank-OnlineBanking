b94e564f4418e28b43406971c980ba2025f60c92
arielpetit <Ariel.Tchikaya@adorsys.com>
2024-12-18 13:09:08 +0100

    chore(): added x-forwarded config manually

--- online-banking-app/src/main/java/com/adorsys/webank/config/ForwardedHeaderConfig.java
+++ online-banking-app/src/main/java/com/adorsys/webank/config/ForwardedHeaderConfig.java
@@ -1,14 +1,14 @@
-package com.adorsys.webank.config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.filter.ForwardedHeaderFilter;
-
-@Configuration
-public class ForwardedHeaderConfig {
-
-    @Bean
-    public ForwardedHeaderFilter forwardedHeaderFilter() {
-        return new ForwardedHeaderFilter();
-    }
-}
+//package com.adorsys.webank.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.filter.ForwardedHeaderFilter;
+//
+//@Configuration
+//public class ForwardedHeaderConfig {
+//
+//    @Bean
+//    public ForwardedHeaderFilter forwardedHeaderFilter() {
+//        return new ForwardedHeaderFilter();
+//    }
+//}
