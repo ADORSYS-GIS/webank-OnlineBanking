a795f98d92474933f5f13508943e2d9785b9c0e5
arielpetit <Ariel.Tchikaya@adorsys.com>
2024-12-18 10:41:07 +0100

    chore(): added the X-fowarded trusting

--- obs/obs-rest/src/main/java/com/adorsys/webank/obs/resource/RegistrationResource.java
+++ obs/obs-rest/src/main/java/com/adorsys/webank/obs/resource/RegistrationResource.java
@@ -2,7 +2,6 @@ package com.adorsys.webank.obs.resource;
 
 import com.adorsys.webank.obs.dto.RegistrationRequest;
 import com.adorsys.webank.obs.service.RegistrationServiceApi;
-import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpStatus;
