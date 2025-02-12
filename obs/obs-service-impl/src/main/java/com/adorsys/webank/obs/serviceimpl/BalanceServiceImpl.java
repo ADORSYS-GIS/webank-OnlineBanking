643e954886c22b7ff1fdba9e4a5f58033b6638b7
NkwaTambe <nkwajudetambe@gmail.com>
2025-02-12 13:15:00 +0100

    Removed  unused import 'java.util.List'.

--- obs/obs-service-impl/src/main/java/com/adorsys/webank/obs/serviceimpl/BalanceServiceImpl.java
+++ obs/obs-service-impl/src/main/java/com/adorsys/webank/obs/serviceimpl/BalanceServiceImpl.java
@@ -1,17 +1,15 @@
 package com.adorsys.webank.obs.serviceimpl;
 
-import com.adorsys.webank.obs.dto.BalanceRequest;
-import com.adorsys.webank.obs.security.JwtCertValidator;
-import com.adorsys.webank.obs.service.BalanceServiceApi;
-import de.adorsys.webank.bank.api.domain.BalanceBO;
-import de.adorsys.webank.bank.api.domain.BankAccountDetailsBO;
-import de.adorsys.webank.bank.api.service.BankAccountService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import com.adorsys.webank.obs.dto.*;
+import com.adorsys.webank.obs.security.*;
+import com.adorsys.webank.obs.service.*;
+import de.adorsys.webank.bank.api.domain.*;
+import de.adorsys.webank.bank.api.service.*;
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.stereotype.*;
 
-import java.time.LocalDateTime;
-import java.util.List;
-import java.util.Optional;
+import java.time.*;
+import java.util.*;
 
 @Service
 public class BalanceServiceImpl implements BalanceServiceApi {
