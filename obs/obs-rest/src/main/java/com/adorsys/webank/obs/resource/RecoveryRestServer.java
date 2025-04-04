ec50ec49e50c83b941e6e56c03cc418fc2aa1293
NkwaTambe <nkwajudetambe@gmail.com>
2025-04-04 14:49:19 +0100

    update recoveryRestServer parameters to properly handle frontnend request

--- obs/obs-rest/src/main/java/com/adorsys/webank/obs/resource/RecoveryRestServer.java
+++ obs/obs-rest/src/main/java/com/adorsys/webank/obs/resource/RecoveryRestServer.java
@@ -40,7 +40,9 @@ public class RecoveryRestServer implements RecoveryRestApi {
 
         String accountId = recoveryDto.getAccountId();
 
-        String accountCert = recoveryServiceApi.recoverAccount(accountId, publicKey.toJSONString());
+        log.info("Recovery request received with accountid: {}", accountId);
+
+        String accountCert = recoveryServiceApi.recoverAccount(publicKey.toJSONString(),accountId);
 
         // Delegate recovery request to the service layer
         if (accountCert != null) {
