15ee51196677a7e6c3c7628712cf3f6c0cdc173e
NkwaTambe <nkwajudetambe@gmail.com>
2025-06-16 13:23:58 +0100

    fix intergration test

--- online-banking-app/src/test/java/com/adorsys/webank/OnlineBankingApplicationTests.java
+++ online-banking-app/src/test/java/com/adorsys/webank/OnlineBankingApplicationTests.java
@@ -3,7 +3,7 @@ package com.adorsys.webank;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = OnlineBankingApplication.class)
 class OnlineBankingApplicationTests {
 
     @Test
