f0261c0b5418dedd25a95cd69776ce1bd189005c
arielpetit <Ariel.Tchikaya@adorsys.com>
2025-06-20 15:01:37 +0100

    chore(): returned the 503 error for the fallback

--- obs/obs-service-impl/src/test/java/com/adorsys/webank/obs/serviceimpl/BalanceServiceImplTest.java
+++ obs/obs-service-impl/src/test/java/com/adorsys/webank/obs/serviceimpl/BalanceServiceImplTest.java
@@ -20,7 +20,7 @@ import java.util.Collections;
 import java.util.Currency;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
 class BalanceServiceImplTest {
