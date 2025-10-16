3103849958cc98063acd313a695aca9945ba0bde
nancymuyeh <nancymuyeh@gmail.com>
2025-10-16 13:07:25 +0100

    hotfix: fix top-up error

--- online-banking-app/src/main/java/com/adorsys/webank/config/WebankReflectionHints.java
+++ online-banking-app/src/main/java/com/adorsys/webank/config/WebankReflectionHints.java
@@ -29,22 +29,31 @@ public class WebankReflectionHints implements RuntimeHintsRegistrar {
 
     // Add explicit critical external classes for reflection (for classes not found by scanning)
     private static final String[] EXTERNAL_CLASSES_TO_REGISTER = {
-//        // Flyway
-//        "org.flywaydb.core.Flyway",
-//        "org.flywaydb.core.api.configuration.FluentConfiguration",
-        // Ledgers mappers
-        "de.adorsys.ledgers.postings.impl.converter.LedgerAccountMapperImpl",
-        "de.adorsys.ledgers.postings.impl.converter.LedgerMapperImpl",
-        "de.adorsys.ledgers.postings.impl.converter.ChartOfAccountMapperImpl",
-        "de.adorsys.ledgers.postings.impl.converter.AccountStmtMapperImpl",
-        "de.adorsys.ledgers.postings.impl.converter.AccountStmtMapper",
-        // Webank domain models
-        "de.adorsys.webank.bank.api.service.domain.ASPSPConfigData",
-        "de.adorsys.webank.bank.api.service.domain.LedgerAccountModel",
-        "de.adorsys.webank.bank.api.service.domain.ClearingAccount",
-        // Webank MapStruct mapper implementation (explicit for native)
-        "de.adorsys.webank.bank.api.service.mappers.BankAccountMapperImpl"
-    };
+ //        // Flyway
+ //        "org.flywaydb.core.Flyway",
+ //        "org.flywaydb.core.api.configuration.FluentConfiguration",
+         // Ledgers mappers
+         "de.adorsys.ledgers.postings.impl.converter.LedgerAccountMapperImpl",
+         "de.adorsys.ledgers.postings.impl.converter.LedgerMapperImpl",
+         "de.adorsys.ledgers.postings.impl.converter.ChartOfAccountMapperImpl",
+         "de.adorsys.ledgers.postings.impl.converter.AccountStmtMapperImpl",
+         "de.adorsys.ledgers.postings.impl.converter.AccountStmtMapper",
+         // Webank domain models
+         "de.adorsys.webank.bank.api.service.domain.ASPSPConfigData",
+         "de.adorsys.webank.bank.api.service.domain.LedgerAccountModel",
+         "de.adorsys.webank.bank.api.service.domain.ClearingAccount",
+         // Webank MapStruct mapper implementation (explicit for native)
+         "de.adorsys.webank.bank.api.service.mappers.BankAccountMapperImpl",
+         "org.hibernate.proxy.HibernateProxy",
+         "org.hibernate.proxy.AbstractLazyInitializer",
+         "org.hibernate.proxy.AbstractPersistentAttributeInterceptable",
+         "org.hibernate.engine.spi.LazyInitializer",
+         // Hibernate proxies for native image
+         "org.hibernate.proxy.HibernateProxy",
+         "org.hibernate.proxy.AbstractLazyInitializer",
+         "org.hibernate.proxy.AbstractPersistentAttributeInterceptable",
+         "org.hibernate.engine.spi.LazyInitializer"
+     };
 
 
     @Override
